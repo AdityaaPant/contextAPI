f39811594cc45a84f5174fb1e6f2522a0979487f
Aditya Pant <pantanupam05@gmail.com>
2025-01-20 12:12:53 +0530

    custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -41,22 +41,47 @@
 // }
 // export default App;
 
-import { useState } from "react";
-import "./App.css";
-function App() {
-	const [count, setCount] = useState(0);
-	function increaseCount() {
-		setCount(count + 1);
-	}
-	function decreaseCount() {
-		setCount(count - 1);
-	}
-	return (
-		<div>
-			<button onClick={increaseCount}>Increse</button>
-			<button onClick={decreaseCount}>decrease</button>
-			<p>{count}</p>
-		</div>
-	);
-}
-export default App;
+// import { useDebugValue, useEffect, useState } from "react";
+// import "./App.css";
+
+// function useCounter() {
+// 	const [count, setCount] = useState(0);
+
+// 	function increaseCount() {
+// 		setCount(count + 1);
+// 	}
+// 	function decreaseCount() {
+// 		setCount(count - 1);
+// 	}
+
+// 	return {
+// 		count,
+// 		increaseCount,
+// 		decreaseCount,
+// 	};
+// }
+// function App() {
+// 	const { count, increaseCount, decreaseCount } = useCounter();
+// 	const [post, setPost] = useState({});
+// 	async function getPosts() {
+// 		const response = await fetch(
+// 			"https://jsonplaceholder.typicode.com/posts/1"
+// 		);
+// 		const json = await response.json();
+// 		setPost(json);
+// 	}
+// 	useEffect(() => {
+// 		getPosts();
+// 	}, []);
+
+// 	return (
+// 		<div>
+// 			<button onClick={increaseCount}>Increese</button>
+// 			<button onClick={decreaseCount}>decraese</button>
+// 			<p>{count}</p>
+// 			<div>{post.title}</div>
+// 		</div>
+// 	);
+// }
+
+// export default App;
